c848d8260efba7d75f84ca0ffe5986bae8ac02aa
Krishnamurtti <118301162+Krishnamurtti@users.noreply.github.com>
2023-05-04 15:15:33 +0200

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -2,5 +2,5 @@
 
 void *memset(void *s, int c, size_t n)
 {
-    if (!s) /* 0 = Falso, 1 = Verdadero. (nulo es falso, no nulo es verdadero) ... !s = s == NULL*/
+    if (!s) /* 0 = Falso, 1 = Verdadero.... !s = s == NULL*/
 }
